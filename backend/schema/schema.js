553e65526a78d02e7486f71df4c9bd4832121b24
LANGROUP\ricq <daniel.ricq@iff.fraunhofer.de>
2023-01-23 16:12:08 +0100

    feat(backend): Add addClient mutation

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,19 +1,38 @@
 const { buildSchema } = require("graphql");
-const { clients } = require("../SampleData.js");
+const { clients, projects } = require("../SampleData.js");
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-    type Client {
-        id: ID,
-        name: String,
-        email: String,
-        phone: String,
-    }
+  type Client {
+    id: ID!,
+    name: String!,
+    email: String!,
+    phone: String!,
+  }
+
+  type Project {
+    id: ID!,
+    clientID: ID!,
+    name: String!
+    description: String!,
+    status: String!
+  }
+
   type Query {
     clients: [Client]
-    client(id: ID!): Client 
+    client(id: ID!): Client
+    projects: [Project]
+    project(id: ID!): Project 
+  }
+
+  type Mutation {
+    addClient(name: String, email: String, phone: String): Client
   }
 `);
 
+// deleteClient(id: ID!)
+// addProject(project: Project!)
+// deleteProject(id: ID!)
+// updateProject(project: Project!)
 // The root provides a resolver function for each API endpoint
 const root = {
   clients: () => {
@@ -21,6 +40,16 @@ const root = {
   },
   client: ({ id }) => {
     return clients.find((client) => client.id === id);
+  },
+  projects: () => {
+    return projects;
+  },
+  project: ({ id }) => {
+    return projects.find((project) => project.id === id);
+  },
+  addClient: (args) => {
+    clients.push({ id: clients.length, ...args });
+    return clients[clients.length - 1];
   },
 };
 
