c547ca2fe1c7e663343d348fd15588b3ee561bf9
Daniel Ricq <dricq@findusapp.de>
2023-02-27 13:10:12 +0100

    feat(client): Add header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,26 @@
-<script setup></script>
+<script setup>
+import BulletinBoard from 'vue-material-design-icons/BulletinBoard.vue'
+</script>
 
 <template>
-  <header></header>
-  <main>Hello World</main>
+  <header>
+    <BulletinBoard title="The project logo" fillColor="var(--primary-color)"></BulletinBoard>
+    Project Mgmt
+  </header>
+  <main style="color: var(--primary)">Hello World</main>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  padding: 0.25rem 5.75rem;
+
+  color: var(--primary-color);
+  background-color: #f8f9fa;
+}
+main {
+  margin-inline: 5.75rem;
+}
+</style>
