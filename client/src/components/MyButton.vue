c5c8b94bb0238f3b02cf27e00c80b3204d43d566
Daniel Ricq <dricq@findusapp.de>
2023-02-28 10:50:36 +0100

    feat(client): Update buttons styles
    - add focus styles
    - add icon variant

--- client/src/components/MyButton.vue
+++ client/src/components/MyButton.vue
@@ -1,21 +1,20 @@
 <script setup>
-const props = defineProps({
+defineProps({
   variant: {
     type: String,
     default: 'text',
     validator(variant) {
       if (!variant) return true
       // The value must match one of these strings
-      return ['primary', 'secondary', 'text', 'error'].includes(variant)
+      return ['primary', 'secondary', 'text', 'error', 'icon'].includes(variant)
     }
   },
-  secondary: Boolean,
-  text: Boolean
+  dense: { type: Boolean, default: false }
 })
 </script>
 
 <template>
-  <button type="button" :class="[variant]"><slot></slot></button>
+  <button type="button" :class="[variant, { dense: dense }]"><slot></slot></button>
 </template>
 
 <style scoped>
@@ -36,13 +35,19 @@ button {
   display: flex;
   align-items: center;
   gap: 0.25rem;
-  padding: 0.25rem 0.5rem;
+  width: max-content;
+  padding: 0.75em;
   border-radius: var(--default-border-radius);
   color: #ffffff;
+  transition: all 0.3s ease;
 }
 
 button:hover {
   opacity: 0.8;
+}
+button:focus {
+  outline: none;
+  box-shadow: 0 0 0 5px rgba(21, 156, 228, 0.4);
 }
 
 .primary {
@@ -55,10 +60,22 @@ button:hover {
 .text {
   color: #000000;
   background-color: #f9fafb;
-  padding: 0.75rem;
 }
 
 .error {
   background-color: var(--error-color);
+}
+.icon {
+  color: hsl(0, 0%, 50%);
+  background-color: unset;
+}
+
+.icon:hover {
+  color: #000000;
+  opacity: 0.75;
+}
+
+.dense {
+  padding: 0.25rem 0.5rem;
 }
 </style>
